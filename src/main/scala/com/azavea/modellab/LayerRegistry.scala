1cb21a7529a02a87d805dd496dd15ebffcc20ffd
Eugene Cheipesh <echeipesh@gmail.com>
2015-10-27 15:25:29 -0400

    add WindowedReader
    also abstracts over making windows around tile requests

--- src/main/scala/com/azavea/modellab/LayerRegistry.scala
+++ src/main/scala/com/azavea/modellab/LayerRegistry.scala
@@ -5,6 +5,8 @@ import spray.json._
 import DefaultJsonProtocol._
 import geotrellis.raster.io.geotiff._
 import geotrellis.raster._
+import geotrellis.spark.io._
+import org.apache.spark.rdd._
 
 import scala.collection.mutable
 
@@ -29,14 +31,17 @@ class LayerRegistry {
   } 
 
   val resize = new ResizeTile(256, 512) // we're reading from DataHub, tiles are too big
+  val window = new Window(2)            // bucffer operation requests by 2 (storage) tiles each direction  
 
   def getTile(guid: GUID, zoom: Int, col: Int, row: Int): Option[Tile] = {
     val requestKey = SpatialKey(col, row)
     val storedKey = resize.getStoredKey(requestKey)
-    val bounds = buffer(storedKey, 1)    
+    val bounds = window.getWindowBounds(storedKey)    
     getLayer(guid).map { window =>
       val tiles = window(zoom, bounds).lookup(storedKey)
       resize.getTile(requestKey, tiles.head)
     }
   }
 }
+
+
---
+++ src/main/scala/com/azavea/modellab/WindowReader.scala
@@ -0,0 +1,70 @@
+package com.azavea.modellab
+
+import geotrellis.spark._
+import spray.json._
+import DefaultJsonProtocol._
+import geotrellis.raster.io.geotiff._
+import geotrellis.raster._
+import geotrellis.spark.io._
+import org.apache.spark.rdd._
+import org.apache.spark.storage._
+import scala.collection.mutable
+
+/**
+ * The purpose of this class is to buffer IO, which benefits from larger windows.
+ * 
+ * - First stage is going to be to get a large window to share IO.
+ * - Second stage is to use the larger window to perform calculation quickly.
+ */
+class WindowedReader(
+  layerReader: FilteringLayerReader[LayerId, SpatialKey, RasterRDD[SpatialKey]], 
+  windowSize: Int = 8, 
+  storageLevel: StorageLevel = StorageLevel.MEMORY_ONLY) extends Window(windowSize) {
+
+  private val windowCache: mutable.HashMap[LayerId, mutable.HashMap[GridBounds, RDD[(SpatialKey, Tile)]]] = 
+    mutable.HashMap.empty
+
+  def getWindow(id: LayerId, key: SpatialKey): RDD[(SpatialKey, Tile)] = {
+    val layerCache = windowCache.getOrElseUpdate(id, mutable.HashMap.empty)
+    val windowBounds = getWindowBounds(key)
+
+    layerCache.synchronized { 
+      layerCache.getOrElseUpdate(windowBounds, 
+        layerReader.query(id).where(Intersects(windowBounds))
+          .toRDD
+          .setName(s"Window::$id::$windowBounds")
+          .persist(storageLevel) )
+    }
+  }
+
+  /** 
+   * If we stradle window boundry, we need to find which windows we intercept.
+   */
+  def getView(id: LayerId, bounds: GridBounds): RDD[(SpatialKey, Tile)] = {
+    val windows = {
+      for {
+        row <- List(bounds.rowMin, bounds.rowMax)
+        col <- List(bounds.colMin, bounds.colMax)
+      } yield getWindow(id, SpatialKey(col, row))
+    }.toSet
+    println(s"Request: $id - $bounds intersects: ${windows.size} windows")
+    windows
+      .map { _.filter { case (key, tile) => bounds.contains(key.col, key.row) } }
+      .reduce( _ union _)
+  }
+}
+
+
+class Window(windowSize: Int) {
+  def getWindowId(key: SpatialKey) = {
+    val SpatialKey(col, row) = key
+    (col / windowSize, row / windowSize)
+  }
+
+  def getWindowBounds(key: SpatialKey) = {
+    val SpatialKey(col, row) = key
+    GridBounds(
+      col - col % windowSize, row - row % windowSize,
+      col + (windowSize - col % windowSize), row + (windowSize - row % windowSize))
+  }   
+}
