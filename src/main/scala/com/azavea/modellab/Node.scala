806dc1da4b5b91d17d6663b4648b3a250ae50955
Eugene Cheipesh <echeipesh@gmail.com>
2015-10-27 15:26:18 -0400

    support focal operations, work around RDD instead of RasterRDD

--- src/main/scala/com/azavea/modellab/Node.scala
+++ src/main/scala/com/azavea/modellab/Node.scala
@@ -6,24 +6,28 @@ import shapeless._
 import shapeless.ops.hlist._
 import geotrellis.spark._
 import geotrellis.spark.io._
+import geotrellis.spark.op.focal._
 import geotrellis.raster._
 import geotrellis.raster.op.local._
-
+import geotrellis.raster.op.focal._
 import org.apache.spark.storage._
 import scala.collection.mutable
+import org.apache.spark.rdd._
 
 object Node {
-  val cache = mutable.HashMap.empty[(Node, Int, GridBounds), RasterRDD[SpatialKey]]
+  val cache = mutable.HashMap.empty[(Node, Int, GridBounds), RDD[(SpatialKey, Tile)]]
 }
 
 trait Node extends Serializable {
-  def calc(zoom: Int, bounds: GridBounds): RasterRDD[SpatialKey]
+  def calc(zoom: Int, bounds: GridBounds): RDD[(SpatialKey, Tile)]
 
-  def apply(zoom: Int, bounds: GridBounds): RasterRDD[SpatialKey] = {
+  def apply(zoom: Int, bounds: GridBounds): RDD[(SpatialKey, Tile)] = {
     // This will backstop calculation of RDDs nodes, hopefully allowing IO nodes to be re-used
     // Critical note: the Node tree will already exist fully.
     val name = s"${this.getClass.getName}::$zoom::$bounds"
     val key = (this, zoom, bounds)
+    
+    // NOTE: We used to cache here to store every layer and their intermidiate results
     Node.cache.get(key) match {
       case Some(rdd) =>
         println(s"Pulled Node: $name")    
@@ -32,7 +36,7 @@ trait Node extends Serializable {
       case None =>
         val rdd = 
           calc(zoom, bounds)
-            .setName(s"${this.getClass.getName}::$zoom::$bounds")
+            .setName(s"${this.getClass.getName}::$zoom::$bounds")           
             .cache
         Node.cache.update(key, rdd)
         println(s"PUSHED Node: $name")  
@@ -41,25 +45,42 @@ trait Node extends Serializable {
   }
 }
 
-case class LoadLayer(layerName: String, layerReader: FilteringLayerReader[LayerId, SpatialKey, RasterRDD[SpatialKey]]) extends Node { 
+case class LoadLayer(layerName: String, layerReader: WindowedReader) extends Node { 
   def calc(zoom: Int, bounds: GridBounds) = {
-    val rdd = layerReader.query(LayerId(layerName, zoom)).where(Intersects(bounds)).toRDD
-    rdd.foreachPartition{ p => () }
-    rdd
+    layerReader.getView(LayerId(layerName, zoom), bounds)
+  }
+}
+
+case class FocalOp(layer: Node, n: Neighborhood, op: (Tile, Neighborhood, Option[GridBounds]) => Tile) extends Node {
+  require(n.extent <= 256, "Maximum Neighborhood extent is 256 cells")
+  def buffer(bounds: GridBounds, cells: Int) = GridBounds(
+    math.max(0, bounds.colMin - cells), 
+    math.max(0, bounds.rowMin - cells),
+    bounds.colMax + cells, 
+    bounds.rowMax + cells)
+
+  def calc(zoom: Int, bounds: GridBounds) = {
+    val extended = buffer(bounds, 1)
+    println(s"FOCAL: $bounds -> $extended")
+    val rdd = layer(zoom, extended)
+    FocalOperation(rdd, Square(1), Some(bounds))(geotrellis.raster.op.focal.Min.apply)
+      .filter { case (key, _) => bounds.contains(key.col, key.row) } // filter buffer tiles, they contain no information      
   }
 }
 
 case class LocalAdd(a: Node, b: Node) extends Node {
   def calc(zoom: Int, bounds: GridBounds) = {
-    a(zoom, bounds).combineTiles(b(zoom, bounds)) { Add(_,_) }
+    a(zoom, bounds).join(b(zoom, bounds)).map { case (key, (tile1, tile2)) =>
+      key -> Add(tile1, tile2)
+    }
   }
 }
 
 case class ValueMask(a: Node, masks: Seq[Int]) extends Node {
   def calc(zoom: Int, bounds: GridBounds) = {
     val _masks = masks
-    a(zoom, bounds).mapTiles { tile =>
-      tile.map { v => if (_masks.contains(v)) NODATA else v }
+    a(zoom, bounds).map { case (key, tile) =>
+      key -> tile.map { v => if (_masks.contains(v)) NODATA else v }
     }
   }    
 }
