064656e5d4dcbf4366e647e8ff883820dbc2ca92
Eugene Cheipesh <echeipesh@gmail.com>
2015-11-03 11:29:18 -0500

    Instrumented trait also contains a logger

--- src/main/scala/com/azavea/modellab/Instrumented.scala
+++ src/main/scala/com/azavea/modellab/Instrumented.scala
@@ -2,6 +2,9 @@ package com.azavea.modellab
 
 import com.librato.metrics._
 import java.util.concurrent._
+import geotrellis.spark.utils.cache._
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 object Instrumented {
   /** The application wide metrics registry. */
@@ -18,5 +21,23 @@ object Instrumented {
 }
 
 trait Instrumented extends nl.grons.metrics.scala.InstrumentedBuilder {
+  val logger = LoggerFactory.getLogger(this.getClass);
   val metricRegistry = Instrumented.metricRegistry
+}
+
+trait InstrumentedCache[K, V] extends Cache[K, V] with Instrumented {
+  private[this] val cacheHit = metrics.counter("hit")
+  private[this] val cacheMiss = metrics.counter("miss")
+
+  abstract override def lookup(k: K):Option[V] = super.lookup(k) match {
+    case None => 
+      cacheMiss += 1
+      logger.debug(s"Cache miss on $k")
+      None
+    
+    case z => 
+      cacheHit += 1
+      logger.debug(s"Cache hit on $k")
+      z
+  }
 }
