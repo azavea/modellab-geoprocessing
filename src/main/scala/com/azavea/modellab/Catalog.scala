80fe80147f72ad1b859de5a80911669ac7a77c6b
Nathan Zimmerman <npzimmerman@gmail.com>
2015-11-23 16:01:04 -0500

    Use typesafe reference config for sensible defaults

--- src/main/scala/com/azavea/modellab/Catalog.scala
+++ src/main/scala/com/azavea/modellab/Catalog.scala
@@ -6,38 +6,44 @@ import geotrellis.spark.io.hadoop._
 import geotrellis.spark.io.s3._
 import geotrellis.spark.io.avro.codecs._
 import geotrellis.spark.io._
+import geotrellis.spark.utils.cache.FileCache
 import geotrellis.spark.io.index._
 
 import org.apache.hadoop.fs.Path
 import org.apache.spark._
 import org.apache.spark.rdd._
 
+import com.typesafe.config._
+
 trait Catalog {
   implicit def sc: SparkContext
-  
-  def layerReader: FilteringLayerReader[LayerId, SpatialKey, RasterRDD[SpatialKey]] 
+
+  def layerReader: FilteringLayerReader[LayerId, SpatialKey, RasterRDD[SpatialKey]]
   def layerWriter : Writer[LayerId, RasterRDD[SpatialKey] with RDD[(SpatialKey, Tile)]]
 }
 
 trait DataHubCatalog extends Catalog with Instrumented {
   implicit def sc: SparkContext
+  private val config = ConfigFactory.load()
 
   private[this] val reading = metrics.timer("read")
 
   private val bucket = "azavea-datahub"
   private val key = "catalog"
 
-  // val cache = (id: LayerId) => new FileCache(s"/Users/eugene/tmp/model-lab/cache/${id.name}/${id.zoom}", _.toString)
+
+  val cacheTemplate = config.getString("cache.location")
+  val cache = (id: LayerId) => new FileCache(cacheTemplate.format(id.name, id.zoom), _.toString)
 
   lazy val layerReader = new S3LayerReader[SpatialKey, Tile, RasterRDD[SpatialKey]](
       new S3AttributeStore(bucket, key),
       new S3RDDReader[SpatialKey, Tile],
-      None) {
-    
+      Some(cache)) {
+
     override val defaultNumPartitions = math.max(1, sc.defaultParallelism)
 
     override def read(id: LayerId, rasterQuery: RDDQuery[SpatialKey, MetaDataType], numPartitions: Int): RasterRDD[SpatialKey] = {
-      reading.time {  
+      reading.time {
         super.read(id, rasterQuery, numPartitions)
       }
     }
